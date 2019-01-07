750a245f6cbaf17ea8885ce67304fc6c88b99cd1
it <it@DESKTOP-6NPBPD7>
2019-01-07 17:48:14 +0900

    delete

---
+++ BoardSevlet/src/action/DeleteAction.java
@@ -0,0 +1,58 @@
+package action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BoardDAO;
+
+/**
+ * Servlet implementation class DeleteAction
+ */
+@WebServlet("/board/delete.bo")
+public class DeleteAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		int num = Integer.parseInt(request.getParameter("num"));
+		String pass = request.getParameter("pass");
+		BoardDAO dao = BoardDAO.getInstance();
+		int a = dao.boardDelete(num,pass);
+		
+		if(a==1) {//비밀번호 불일치
+			request.setAttribute("a", 1);
+			RequestDispatcher rd = request.getRequestDispatcher("passCheck.jsp?BOARD_NUM="+num+"&checkNum=2");
+			rd.forward(request, response);
+			
+		}else if(a==2){//비밀번호 일치
+			response.sendRedirect("list.bo");	
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
