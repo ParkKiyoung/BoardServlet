e21a6d5712afe4518cb7e45a428abf24ddd98b5e
it <it@DESKTOP-6NPBPD7>
2019-01-07 15:21:01 +0900

    list, boardView

--- BoardSevlet/src/dao/BoardDAO.java
+++ BoardSevlet/src/dao/BoardDAO.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -30,7 +33,7 @@ public class BoardDAO {
 		
 		try {
 			con = getConnection();
-			String sql = "insert into board values(board_seq.nextval,?,?,?,?,?,?,?,?,?,sysdate)";
+			String sql = "insert into board values(board_seq.nextval,?,?,?,?,?,board_seq.nextval,?,?,?,sysdate)";
 			ps = con.prepareStatement(sql);
 			ps.setString(1, bb.getBOARD_NAME());
 			ps.setString(2, bb.getBOARD_PASS());
@@ -40,7 +43,6 @@ public class BoardDAO {
 			ps.setInt(6, 0);
 			ps.setInt(7, 0);
 			ps.setInt(8, 0);
-			ps.setInt(9, 0);
 			ps.executeQuery();
 			
 		}catch(Exception e) {
@@ -59,5 +61,82 @@ public class BoardDAO {
 			e.printStackTrace();
 		}
 		
+	}
+
+	public ArrayList<BoardBean> boardList() {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		ArrayList<BoardBean> arr = new ArrayList<>();
+		try {
+			con = getConnection();
+			String sql = "select * from board";
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				BoardBean bb = new BoardBean();
+				bb.setBOARD_NUM(rs.getInt("board_num"));
+				bb.setBOARD_CONTENT(rs.getString("board_content"));
+				bb.setBOARD_DATE(rs.getDate("board_date"));
+				bb.setBOARD_FILE(rs.getString("board_file"));
+				bb.setBOARD_NAME(rs.getString("board_name"));
+				bb.setBOARD_PASS(rs.getString("board_pass"));
+				bb.setBOARD_RE_LEV(rs.getInt("re_lev"));
+				bb.setBOARD_RE_REF(rs.getInt("re_ref"));
+				bb.setBOARD_READCOUNT(rs.getInt("board_readcount"));
+				bb.setBOARD_SUBJECT(rs.getString("board_subject"));
+				arr.add(bb);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			closeCon(con, st,rs);
+		}
+		return arr;
+	}
+
+	private void closeCon(Connection con, Statement st, ResultSet rs) {
+		try {
+			if(con!=null)con.close();
+			if(st!=null)st.close();
+			if(rs!=null)rs.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public BoardBean BoardView(int num) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		BoardBean bb = null;
+		String sql = "";
+		try {
+			con = getConnection();
+			st = con.createStatement();
+			sql = "update board set board_readcount = board_readcount+1 where board_num = "+num;
+			st.executeUpdate(sql);
+			sql = "select * from board where BOARD_NUM = "+num;
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				bb = new BoardBean();
+				bb.setBOARD_NUM(rs.getInt("board_num"));
+				bb.setBOARD_CONTENT(rs.getString("board_content"));
+				bb.setBOARD_DATE(rs.getDate("board_date"));
+				bb.setBOARD_FILE(rs.getString("board_file"));
+				bb.setBOARD_NAME(rs.getString("board_name"));
+				bb.setBOARD_PASS(rs.getString("board_pass"));
+				bb.setBOARD_RE_LEV(rs.getInt("re_lev"));
+				bb.setBOARD_RE_REF(rs.getInt("re_ref"));
+				bb.setBOARD_READCOUNT(rs.getInt("board_readcount"));
+				bb.setBOARD_SUBJECT(rs.getString("board_subject"));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			closeCon(con, st,rs);
+		}
+		return bb;
 	}
 }
